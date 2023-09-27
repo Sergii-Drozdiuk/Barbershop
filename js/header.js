41bb7f9d4574496fa42841eff474e2172405d650
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-09-27 12:15:19 +0300

    Fix-bugs

--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
 window.onscroll = () => changeHeaderBackground();
 
 function changeHeaderBackground() {
-  const header = document.querySelector('[data-header]');
+  const header = document.querySelector("[data-header]");
   const headerOffsetTrigger = header.offsetHeight;
   const pageOffset = window.pageYOffset;
 
   if (pageOffset > headerOffsetTrigger) {
-    header.classList.add('no-transparency');
+    header.classList.add("no-transparency");
   } else {
-    header.classList.remove('no-transparency');
+    header.classList.remove("no-transparency");
   }
 }
--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,30 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const modal = document.getElementById("modal");
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const modalBtn = document.querySelectorAll(".open-modal");
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+const closeBtn = document.getElementsByClassName("modal-close-btn")[0];
+
+// Listen 	for OPEN Click
+modalBtn.forEach(function (e) {
+  e.addEventListener("click", openModal);
+});
+// Listen for CLOSE Click
+closeBtn.addEventListener("click", closeModal);
+// Listen for OUTSIDE Click
+window.addEventListener("click", outsideClick);
+
+// Function to OPEN modal
+function openModal() {
+  modal.classList.remove("is-hidden");
+}
+
+// Function to CLOSE modal
+function closeModal() {
+  modal.classList.add("is-hidden");
+}
+// Function to CLOSE modal
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.classList.add("is-hidden");
   }
-})();
+}
